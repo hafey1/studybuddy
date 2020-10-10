def30ea50edf49a330b7aba13958cf6f9a103fae
TheFossilDev <colewestbrookphoto@gmail.com>
2020-10-09 23:07:12 -0400

    integrated-ViewRepo-precursor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Resources from './resources';
+import ViewRepo from './ViewRepo'
+import TableTest from './TableTest'
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 
 class App extends Component {
   constructor() {
@@ -50,7 +62,7 @@ class App extends Component {
 
     const items = Resources.map(data=>{
       return(
-      
+        <div>
         <ul>
           <li style={{position:'relative',left:'10vh'}}>
             <span>{data.name}</span>
@@ -58,21 +70,25 @@ class App extends Component {
             <a href={JSON.stringify(data.link)}>{JSON.stringify(data.link)}</a>
           </li>
         </ul>
+        </div>
+        
       )
     })
     return (
       <div className="parent">
+        
         <nav><button>Logo</button>Sign in</nav>
         <div className="container">
         
           <aside style={{backgroundColor: "whitesmoke"}}>Friends<p>update Feed</p><br></br></aside>
-          <div className="searchbar">
-            <input 
+          { 
+            this.state.landingPage ? <div className="searchbar">
+             <input 
               type="text" 
               placeholder="Enter item to be searched" 
               onKeyPress={(e)=>this.searchResource(e)} />
             <button onClick={()=>{console.log(this.state.searchResult)}}>Trouble</button>
-            </div>
+          </div > : <TableTest></TableTest> }
         </div>
         <footer>footer</footer>
       </div>
